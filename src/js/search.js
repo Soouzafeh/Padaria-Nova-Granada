7acd55815a85de65d1c7134dd8e45f2de7bf6913
unknown <felipefdsouza@gmail.com>
2021-11-16 01:25:13 -0300

    Tudo quase pronto, só adicionar itens da sugestão

--- src/js/search.js
+++ src/js/search.js
@@ -1,3 +1,6 @@
+
+//customização
+
 const allProducts = [
     /*
 
@@ -11,40 +14,88 @@ const allProducts = [
     */
 
     {
-        name: "Produto A",
+        name: "Bolo de Morango",
         img: "",
-        link: "/product/a"
+        link: "/encomendas.html"
+    },
+    {
+        name: "Produto B",
+        img: "",
+        link: "/encomendas.html"
     },
 ]
 
-function ClearResults(){
+let itemsLimit = 4;
 
+//end customização
+
+//methods
+
+function ClearResults() {
+    $(".search-item").remove()
 }
 
-function OnSearch(inputText){
+function OnSearch(inputText) {
     ClearResults()
 
-    if(!(inputText instanceof String)) return;
-
-    if(!inputText) {
+    if (!inputText) {
+        $("#searchBox").removeAttr("style")
+        $("#search-suges").css("display", "none");
         return;
-    }else if(inputText.length < 2) {return;}
-    else{
+    } else if (inputText.length < 2) {
+        $("#searchBox").removeAttr("style")
+        $("#search-suges").css("display", "none");
+        return;
+    }
+    else {
+
+        $("#searchBox").css("width", "17.569546120058565vw");
+        $("#searchBox").css("padding", "0 0.43923865300146414vw");
+
+        $("#search-suges").css("display", "flex");
+
         const filteredProducts = allProducts.filter(
             (product) => {
-                const words = product.name.split(" ");
-                for(var i = 0; i < words.length; i++){
+                const words = product.name.toLowerCase().split(" ");
+                for (var i = 0; i < words.length; i++) {
                     const word = words[i];
-                    if(word.startsWith(inputText)) return true;
+                    if (word.startsWith(inputText.toLowerCase())) return true;
                     continue;
                 }
-                return false;
-            }
-        ).sort((a, b) => {return a.name - b.name}).slice(0, 4)
 
-        for(var i = 0; i < filteredProducts.length; i++){
+                return product.name.toLowerCase().startsWith(inputText.toLowerCase());
+            }
+        ).sort((a, b) => { return a.name - b.name }).slice(0, itemsLimit)
+
+        if (filteredProducts.length === 0) {
+            $("#searchBox").removeAttr("style")
+            $("#search-suges").css("display", "none");
+            return;
+        }
+
+        for (var i = 0; i < filteredProducts.length; i++) {
             const product = filteredProducts[i];
-            if(!product) continue;
+            if (!product) continue;
+
+            $("#search-suges").append(`
+            <a href="${product.link}">
+                <div class="search-item" id="search-item">
+                    <img height="64px" src="${product.img}">
+                    <div class="info">
+                        <p>${product.name}</p>
+                    </div>
+                </div>
+            </a>
+            `)
+
         }
     }
 }
+
+$("#searchBox").on("input", (e) => {
+    const value = e.target.value.trim();
+    OnSearch(value);
+})
+
+$("#searchBox").removeAttr("style")
+$("#search-suges").css("display", "none");
